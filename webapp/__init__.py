e0b118211b33afadd1582ec473fff4c01bae1059
Sergey Buzhak <byzhak@ya.ru>
2021-07-23 00:55:12 +0300

    added forms

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from webapp.forms import ItemForm
 
 def create_app():
 
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
 
     @app.route('/')
     def index():
-        return 'Hello'
+        title = 'Калькулятор цен заказа'
+        item_form = ItemForm()
+        return render_template("index.html", page_title=title, form=item_form)
+
+    @app.route('/process_add', methods=['POST'])
+    def process_add():
+        None
+        return redirect(url_for('index'))
+    
 
     return app
