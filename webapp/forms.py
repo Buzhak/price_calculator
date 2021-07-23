a6fe52ee2941b9465b943a55030a885636df8e36
Sergey Buzhak <byzhak@ya.ru>
2021-07-24 00:24:54 +0300

    addad calculator and functions

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, RadioField, SelectField, SubmitField
 
 class ItemForm(FlaskForm):
-    material = SelectField('Выберите материал', choices=[('1', 'фанера'), ('2', 'акрил'), ('3', 'полистирол')])
-    thickness = RadioField('Толщина материала', choices=[('3', '3мм'), ('4', '4мм'), ('6', '6мм'), ('8', '8мм')])
+    material = SelectField('Выберите материал')
     long = IntegerField('Длинна реза в метрах')
-    submit = SubmitField('Добавить и рассчитать')
+    submit = SubmitField('Добавить и рассчитать', render_kw={'class':'btn btn-primary'})
---
+++ webapp/scripts.py
@@ -0,0 +1,38 @@
+import re
+
+
+PRICE_MATERIAL = {
+    "акрил-3мм":30,
+    "акрил-4мм":40,
+    "акрил-5мм":50,
+    "акрил-6мм":60,
+    "акрил-8мм":100,
+    "акрил-10мм":120,
+    "зеркальный-полистирол-2мм":35,
+    "зеркальный-полистирол-3мм":45,
+    "полистирол-3мм":30,
+    "полистирол-4мм":40,
+    "фанера-3мм":30, 
+    "фанера-4мм":35,
+    "фанера-6мм":60,
+    "фанера-8мм":80
+    }
+COST_FILE_EDIT = 300
+MIN_COST = 600
+
+def price() -> list:
+    return PRICE_MATERIAL.keys()
+
+def serialize_price() -> list:
+    return [(name, name) for name in price()]
+
+def cost_calc(cutting_len: int, material: str) -> int:
+    return cutting_len * PRICE_MATERIAL[material]
+
+def sum_prices(prices: list) -> int:
+    
+    cost = sum([price[2] for price in prices])
+    cost += cost/100*10+COST_FILE_EDIT # +10% + working with a file
+    if cost < MIN_COST:
+        cost = MIN_COST
+    return cost
